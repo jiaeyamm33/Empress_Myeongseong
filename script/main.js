1cd21617f2ba576eb66ba55d4c21e85a9182bf75
xnclxud9 <xnclxud9@gmail.com>
2023-02-27 11:29:26 +0900

    emart
    ++

---
+++ script/main.js
@@ -0,0 +1,46 @@
+
+// 스크롤
+window.addEventListener("wheel", function(e){
+	e.preventDefault();
+},{passive : false});
+
+var $html = $("html");
+var page = 1;
+var lastPage = $("section").length;
+
+$html.animate({scrollTop:0},10);
+
+$(window).on("wheel", function(e){
+
+  if($html.is(":animated")) return;
+
+  if(e.originalEvent.deltaY > 0){
+    if(page == lastPage) return;
+
+    page++;
+  }else if(e.originalEvent.deltaY < 0){
+    if(page == 1) return;
+
+    page--;
+  }
+  var posTop = (page-1) * $(window).height();
+
+  $html.animate({scrollTop : posTop});
+
+});
+
+
+// 매장찾기
+$(function(){
+
+  $('.store_info li img').hover(function(){
+    let src = $(this).attr('src').replace('png','gif');
+    $(this).attr('src', src);
+  });
+
+  $('.store_info li img').mouseleave(function(){
+    let src = $(this).attr('src').replace('gif','png');
+    $(this).attr('src', src);
+  });
+
+});
