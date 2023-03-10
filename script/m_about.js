95dc48c79a4890bcb7c02782b6fc7a43ba92bb51
woooooohh1 <dngus1703@gmail.com>
2023-03-10 17:29:43 +0900

    haha
    haha

--- script/m_about.js
+++ script/m_about.js
@@ -10,8 +10,8 @@ $(function(){
  let tab_mnu = $('#relc .logis li .tab');
  console.log(tab_mnu);
  tab_mnu.click(function() {
-  $(this).addClass('on').parent().siblings().find('a').removeClass('on');
-  $(this).next().show();
+  $(this).toggleClass('on').parent().siblings().find('a').removeClass('on');
+  $(this).next().slideToggle();
   $(this).parent().siblings().find('.tab').next().hide();
   })
 
