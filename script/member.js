901e4131388c9f0827f7a40efe9c47cd115d822e
xnclxud9 <xnclxud9@gmail.com>
2023-03-14 17:02:46 +0900

    emart

--- script/member.js
+++ script/member.js
@@ -1,15 +1,15 @@
 $(function(){
-  let t_mnu = $('.join2 > li > a');
+  let t_mnu = $('.join2 > li');
 
   t_mnu.click(function(){
 
     //a태그 자손 i.fas에 적용되는 서식
-    $(this).find('.arrow').toggleClass('a_on').toggleClass('a_off').parents().siblings().find('.a_off').removeClass('a_off').addClass('a_on');
-
-    $(this).find('.arrow2').toggleClass('a_on').toggleClass('a_off').parents().siblings().find('.a_off').removeClass('a_off').addClass('a_on');
+    $('.arrow').removeClass('a_on');
+    $(this).$('.arrow').toggleClass('a_on');
 
     //a태그의 자손 .cont에 적용되는 서식
-    $(this).next().slideToggle().parent().siblings().find('.ser_box').slideUp();
+    $(this).find('.ser_box').slideToggle().siblings().find('.ser_box').slideUp();
+
     return false;
   });
 
